656e394e8cd1cb64fafdd94ced692c993e0485bf
Gianmarco <gianmarco.giretti@htwg-konstanz.de>
2018-05-02 11:04:26 +0200

    extended ranking

--- resources/views/bewerten.blade.php
+++ resources/views/bewerten.blade.php
@@ -1,6 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <p></p>
+                <h3>Aktivitäten</h3>
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Aktivität</th>
+                        <th>Standort</th>
+                        <th>Ranking</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($hs as $h)
+                        <tr>
+                            <td> {{  $h->aktivitaet }} </td>
+                            <td> {{  $h->standort }}</td>
+                            <td> {{  $h->ranking }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 @endsection
