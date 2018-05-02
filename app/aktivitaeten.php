ec46fab1539046176a2354c5ddf0bcd208ce4e03
rene <borner.rene@gmail.com>
2018-05-02 11:35:08 +0200

    fixed aktivitaeten

---
+++ app/aktivitaeten.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class aktivitaeten extends Model
+{
+    protected $table = 'aktivitaeten';
+}
---
+++ app/vorlesungen.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rene
+ * Date: 02.05.18
+ * Time: 11:11
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class vorlesungen extends Model
+{
+    protected $table = 'vorlesungen';
+}
