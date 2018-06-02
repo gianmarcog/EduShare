1ede530432a691157c2b0b8f5b774fa06e4aa519
René Borner <rene.borner@htwg-konstanz.de>
2018-06-02 21:08:22 +0200

    started with rating

--- resources/views/bewerten.blade.php
+++ resources/views/bewerten.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1 class="top-buffer">Hallo {{Auth::user()->name}}!</h1>
+                        <h4>Du kannst folgendes bewerten: </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
