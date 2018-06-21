592764d0037cb43487f7083d624e2030a0b940ae
René Borner <rene.borner@htwg-konstanz.de>
2018-06-21 09:11:38 +0200

    clickable images on welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,21 +12,28 @@
         </ol>
         <div class="carousel-inner" role="listbox">
             <div class="item carousel-item active">
-                <img class="d-block img-fluid objectfit" src="/image/Htwg.jpg" alt="First slide">
+                <a href="{{url('/hochschule/1')}}">
+                    <img class="d-block img-fluid objectfit" src="/image/Htwg.jpg" alt="First slide">
+                </a>
                 <div class="carousel-caption d-none d-md-block">
                     <h1>HTWG Konstanz</h1>
                     <h5>Studieren wo andere Urlaub machen</h5>
                 </div>
             </div>
             <div class="item carousel-item">
-                <img class="d-block img-fluid objectfit" src="/image/aktivitaeten/2.jpg" alt="Second slide">
+                <a href="{{url('/aktivitaet/2')}}">
+                    <img class="d-block img-fluid objectfit" src="/image/aktivitaeten/2.jpg" alt="Second slide">
+                </a>
                 <div class="carousel-caption d-none d-md-block">
                     <h1>Fußballplatz Konstanz</h1>
-                    <h5>Der Fußballplatz mitten in Konstanz. Der perfekte Ort um Turniere zu veranstalten und Tag und Nacht Fußball zu spielen</h5>
+                    <h5>Der Fußballplatz mitten in Konstanz. Der perfekte Ort um Turniere zu veranstalten und Tag und
+                        Nacht Fußball zu spielen</h5>
                 </div>
             </div>
             <div class="item carousel-item">
-                <img class="d-block img-fluid objectfit" src="/image/Berrys.jpg" alt="Third slide">
+                <a href="{{url('/aktivitaet/3')}}">
+                    <img class="d-block img-fluid objectfit" src="/image/Berrys.jpg" alt="Third slide">
+                </a>
                 <div class="carousel-caption d-none d-md-block">
                     <h1>Berry's Konstanz</h1>
                     <h5>Create your own story. Wir sind da, um deine Nächte unvergesslich zu machen.</h5>
@@ -48,21 +55,27 @@
         <h3>Was kannst du auf EduShare machen?</h3><br>
         <div class="row">
             <div class="col-sm-4">
-                <img src="/image/forum.jpg" class="img-responsive rounded" style="width:100%" alt="Image">
+                <a href="{{route('release')}}">
+                    <img id="img100" src="/image/forum.jpg" class="img-responsive rounded" alt="Image">
+                </a>
                 <h4>Forum</h4>
                 <div class="col-12">
                     <p>Wenn du Fragen hast kannst du sie hier deinen Kommilitonen stellen.</p>
                 </div>
             </div>
             <div class="col-sm-4">
-                <img src="/image/basketball.jpg" class="img-responsive rounded" style="width:100%" alt="Image">
+                <a href="{{route('aktivitaeten')}}">
+                    <img id="img100" src="/image/basketball.jpg" class="img-responsive rounded" alt="Image">
+                </a>
                 <h4>Aktivitäten</h4>
                 <div class="col-12">
                     <p>Finde eine Hochschule die dir deine Freizeitaktivitäten bietet.</p>
                 </div>
             </div>
             <div class="col-sm-4">
-                <img src="/image/bewertung.jpg" class="img-responsive rounded" style="width:100%" alt="Image">
+                <a href="{{route('bewerten')}}">
+                    <img id="img100" src="/image/bewertung.jpg" class="img-responsive rounded" alt="Image">
+                </a>
                 <h4>Bewerten</h4>
                 <div class="col-12">
                     <p>Teile deine Erfahrungen mit einem Professor oder einer Vorlesung und bewerte sie!</p>
