b6703c1d2cc034a17ffe070a12d8870002a6ed91
retat <borner.rene@gmail.com>
2018-06-21 15:54:54 +0200

    Auth Check for Forum and Formulation fix of Turniere

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,11 +34,17 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="/js/app.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
 <script src="/js/cookies.js"></script>
 <script src="/js/loading-bar.js"></script>
 <script src="/js/liveSearch.js"></script>
 
+<script>
+    $('.pagination li').addClass("page-item");
+    $('.pagination a').addClass("page-link");
+    $('.pagination span').addClass("page-link");
+</script>
 @yield('scripts')
 
 </body>
