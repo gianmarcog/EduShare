f6054f64827b2adfd58771bc3c12eab8672c2e59
Gianmarco <gianmarco.giretti@htwg-konstanz.de>
2018-06-25 15:38:50 +0200

    trying to fix it

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-site-verification" content="EthldYoBjtVdmUeCcbk_ZndULxHL11PKIvrd9T82GL0" />
-    <meta name="description" content="EduShare - Die Platform für Studenten und Studieninteressierte"/>
+    <meta name="description" content="EduShare - Die Studenten Plattform von Studenten für Studenten!"/>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -16,9 +16,9 @@
     <link rel="icon" href="/image/Logosmall.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/combinedCss.css">
     <link rel="stylesheet" type="text/css"
           href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css"/>
-    <link rel="stylesheet" href="/css/combinedCss.css">
 
 
 </head>
@@ -32,9 +32,10 @@
 <!-- Scripts -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="/js/app.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
-<script src="/js/combinedJS.js"></script>
+<script src="/js/combinedJS"></script>
 @yield('scripts')
 
 </body>
