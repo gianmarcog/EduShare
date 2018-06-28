5771c73a7cd20650b96963dce5caea4810a55fee
René Borner <rene.borner@htwg-konstanz.de>
2018-06-28 20:17:57 +0200

    Enabled Registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -84,10 +84,9 @@
                                        name="password_confirmation" required>
                             </div>
                         </div>
-                        <h3>Derzeit erlauben wir keine neuen Accounts!</h3>
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary" disabled>
+                                <button type="submit" class="btn btn-primary">
                                     Registrieren
                                 </button>
                             </div>
